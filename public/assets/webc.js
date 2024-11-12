71fe27aa0a20cefd4de2f95e4072a79fcbde8a52
kev_durber <kevin.durber@podium-solutions.co.uk>
2024-11-12 17:13:02 +0000

    Add various deployment scripts

--- public/assets/webc.js
+++ public/assets/webc.js
@@ -1,34 +1,52 @@
-class PodiumPCW extends HTMLElement {  
+
+
+class PodiumPCW extends HTMLElement {
+  baseUrl = "/";
+  manifestUrl = this.baseUrl + "assets/manifest.json";
+
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: "open" });
 
-    const target = document.querySelector('podium-pcw');
+    const target = document.querySelector("podium-pcw");
     const host = document.createElement("div");
     host.id = "app";
     target.insertAdjacentElement("afterend", host);
 
-    this.fetchSiteAssets();
+    this.fetchSiteAssets()
+  }
+
+  showError(shadowRoot) {
+    const errorElement = document.createElement("p");
+    errorElement.textContent = "Woof.";
+    shadowRoot.appendChild(errorElement);
   }
 
   async fetchSiteAssets() {
-    const source = "/";
-    const manifest = await fetch(source + "assets/manifest.json");
-    const data = await manifest.json();
-  
-    const indexJs = data["index.html"].file;
-  
-    const script = document.createElement("script");
-    script.type = "module";
-    script.src = source + indexJs;
-  
-    document.head.appendChild(script);
-  
-    data["index.html"].css.forEach((item) => {
-      const styles = document.createElement("link");
-      styles.setAttribute("rel", "stylesheet");
-      styles.setAttribute("href", source + item);
-      document.head.appendChild(styles);
+    return new Promise(async (resolve, reject) => {
+      const manifest = await fetch(this.manifestUrl).catch(() => {
+        this.showError();
+        reject();
+      });
+
+      const data = await manifest.json();
+
+      const indexJs = data["index.html"].file;
+
+      const script = document.createElement("script");
+      script.type = "module";
+      script.src = this.baseUrl + indexJs;
+
+      document.head.appendChild(script);
+
+      data["index.html"].css.forEach((item) => {
+        const styles = document.createElement("link");
+        styles.setAttribute("rel", "stylesheet");
+        styles.setAttribute("href", this.baseUrl + item);
+        document.head.appendChild(styles);
+      });
+
+      resolve();
     });
   }
 }
